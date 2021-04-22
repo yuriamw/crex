29d1ddf75fdd41a50f2c52045a64903383310a87
yuriamw <yuri.amw@gmail.com>
2021-04-23 00:22:27 +0300

    Add filename to logger

--- logger.h
+++ logger.h
@@ -4,6 +4,6 @@
 #include <QDebug>
 #include <QString>
 
-#define TRACE(msg) qDebug() << QString("%1():%2:").arg(__FUNCTION__).arg(__LINE__).toLatin1().data() << msg
+#define TRACE(msg) qDebug() << QString("%1: %2():%3:").arg(__FILE__).arg(__FUNCTION__).arg(__LINE__).toLatin1().data() << msg
 
 #endif // LOGGER_H
